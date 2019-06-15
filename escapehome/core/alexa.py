2e88cf9ee980c080145b12038a9f7da340247e3c
Anna Abad Sieper <a.abadsieper@aoe.com>
2019-06-15 14:45:04 +0200

    add count flashs intent

--- escapehome/core/alexa.py
+++ escapehome/core/alexa.py
@@ -5,10 +5,11 @@ HOUSES = ("gryffindor", "hufflepuff", "ravenclaw", "slytherin")
 
 @intent(app='escapehome')
 def LaunchRequest(session):
+    f = open("core/alexa-resource/welcome-text.txt", "r")
+    reprompt = f.readline()
+    f.close()
     return ResponseBuilder.create_response(message="Willkommen im Escape Room!",
-                                           reprompt="Finde den Code heraus, welcher die Box öffnet.",
-                                           end_session=False,
-                                           launched=True)
+                                           reprompt=reprompt, end_session=False, launched=True)
 
 
 class PointsForHouseSlots(fields.AmazonSlots):
@@ -24,3 +25,18 @@ def PointsForHouse(session, house, points):
         kwargs['end_session'] = False
         kwargs['launched'] = session['launched']
     return ResponseBuilder.create_response(**kwargs)
+
+
+class CountFlashsSlots(fields.AmazonSlots):
+    counter = fields.AmazonNumber()
+
+
+@intent(slots=CountFlashsSlots, app='escapehome')
+def CountFlashs(session, counter):
+    kwargs = {'message': f"{counter} Mal hast du gezählt."}
+    if session.get('launched'):
+        kwargs['reprompt'] = "Wie oft blinkt die Lampe?"
+        kwargs['end_session'] = False
+        kwargs['launched'] = session['launched']
+    return ResponseBuilder.create_response(**kwargs)
+
