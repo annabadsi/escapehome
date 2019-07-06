ed3a6a423f3b7bb3ffbf3b4c56496097a2fe6b35
Anna Abad Sieper <anna.alicia@freenet.de>
2019-07-06 15:13:55 +0200

    add logging

--- box/master-modbus-magnet.py
+++ box/master-modbus-magnet.py
@@ -6,13 +6,21 @@ import time
 PIN = 15
 IP_ADDRESS = '192.168.137.151'
 
+FORMAT = ('%(asctime)-15s %(threadName)-15s '
+'%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 def write_via_tcp_modbus(state):
-    client = ModbusTcpClient(IP_ADDRESS)
-    client.write_coil(1, state)
-    result = client.read_coils(1, 1)
-    print result.bits[0]
-    client.close()
+    try:
+        client = ModbusTcpClient(IP_ADDRESS, port=502, timeout=10)
+        client.write_coil(1, state)
+        result = client.read_coils(1, 1)
+        print result.bits[0]
+        client.close()
+    except RuntimeError as err:
+         log.debug(err)
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
