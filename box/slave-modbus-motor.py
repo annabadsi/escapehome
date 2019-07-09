942197a283be82a5e45b4f20431c7eb42849ecdb
Abad Sieper, Anna Alicia <a.abadsieper@aoe.com>
2019-07-09 16:15:42 +0200

    modbus master check magnet

--- box/slave-modbus-motor.py
+++ box/slave-modbus-motor.py
@@ -1,92 +1,72 @@
 #!/usr/bin/env python
-"""
-Pymodbus Server With Updating Thread
---------------------------------------------------------------------------
-This is an example of having a background thread updating the
-context while the server is operating. This can also be done with
-a python thread::
-    from threading import Thread
-    thread = Thread(target=updating_writer, args=(context,))
-    thread.start()
-"""
-# --------------------------------------------------------------------------- #
-# import the modbus libraries we need
-# --------------------------------------------------------------------------- #
+
 from pymodbus.server.asynchronous import StartTcpServer
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
-
-# --------------------------------------------------------------------------- #
-# import the twisted libraries we need
-# --------------------------------------------------------------------------- #
 from twisted.internet.task import LoopingCall
-
-# --------------------------------------------------------------------------- #
-# configure the service logging
-# --------------------------------------------------------------------------- #
+import RPi.GPIO as GPIO
 import logging
+
 FORMAT = ('%(asctime)-15s %(threadName)-15s'
-	  ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-# --------------------------------------------------------------------------- #
-# define your callback process
-# --------------------------------------------------------------------------- #
-
-import RPi.GPIO as GPIO
-import time
-
-
-PIN = 12
+SERVO_PIN = 12
+MAGNET_PIN = 15
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PIN, GPIO.OUT)
-servo = GPIO.PWM(PIN, 50)
-state = True
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setup(MAGNET_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+servo = GPIO.PWM(SERVO_PIN, 50)
+motor_opend = True
+magnet_opend = False
 
 
-def updating_writer(a):
-    """ A worker process that runs every so often and
-    updates live values of the context. It should be noted
-    that there is a race condition for the update.
-    :param arguments: The input arguments to the call
-    """
-    global state
-    log.debug("read coil")
+def updating_motor(a):
+    global motor_opend
     context = a[0]
     register = 1
     slave_id = 0x00
     address = 0x01
     values = context[slave_id].getValues(register, address, count=1)
-    if values[0] != state:
-	state = values[0]
-        if state == True:
-            log.debug("Drehung auf 90 Grad (box auf)")
-            servo.ChangeDutyCycle(7.5)
+    if values[0] != motor_opend:
+        motor_opend = values[0]
+        if motor_opend == True:
+            log.debug("Drehung auf 0 Grad (box auf)")
+            servo.ChangeDutyCycle(2.5)
         else:
-            log.debug("Drehung auf 180 Grad (box zu)")
-            servo.ChangeDutyCycle(12.5)
+            log.debug("Drehung auf 90 Grad (box zu)")
+            servo.ChangeDutyCycle(7.5)
+
+
+def updating_magnet_in_context(a):
+    global magnet_opend
+    new_value = GPIO.input(MAGNET_PIN)
+    log.debug("check if box is opend or closed by magnet")
+    if new_value != magnet_opend:
+        log.debug("set magnet in context 0x02")
+        context = a[0]
+        register = 1
+        slave_id = 0x00
+        address = 0x02
+        values = [new_value]
+        context[slave_id].setValues(register, address, values)
 
 
 def run_updating_server():
-    # ----------------------------------------------------------------------- # 
-    # initialize your data store
-    # ----------------------------------------------------------------------- # 
-    
+    # initialize data store
     store = ModbusSlaveContext(
-        di=ModbusSequentialDataBlock(0, [17]*100),
-        co=ModbusSequentialDataBlock(0, [17]*100),
-        hr=ModbusSequentialDataBlock(0, [17]*100),
-        ir=ModbusSequentialDataBlock(0, [17]*100))
+        di=ModbusSequentialDataBlock(0, [17] * 100),
+        co=ModbusSequentialDataBlock(0, [17] * 100),
+        hr=ModbusSequentialDataBlock(0, [17] * 100),
+        ir=ModbusSequentialDataBlock(0, [17] * 100))
     context = ModbusServerContext(slaves=store, single=True)
-    
-    # ----------------------------------------------------------------------- # 
+
     # initialize the server information
-    # ----------------------------------------------------------------------- # 
     identity = ModbusDeviceIdentification()
     identity.VendorName = 'pymodbus'
     identity.ProductCode = 'PM'
@@ -94,13 +74,11 @@ def run_updating_server():
     identity.ProductName = 'pymodbus Server'
     identity.ModelName = 'pymodbus Server'
     identity.MajorMinorRevision = '2.2.0'
-    
-    # ----------------------------------------------------------------------- # 
-    # run the server you want
-    # ----------------------------------------------------------------------- # 
-    time = 0.5  # 5 seconds delay
-    loop = LoopingCall(f=updating_writer, a=(context, ))
-    loop.start(time, now=False) # initially delay by time
+
+    motor_loop = LoopingCall(f=updating_motor, a=(context,))
+    magnet_loop = LoopingCall(f=updating_magnet_in_context, a=(context,))
+    motor_loop.start(0.5, now=False)
+    magnet_loop.start(0.1, now=False)
     StartTcpServer(context, identity=identity, address=("0.0.0.0", 502))
 
 
