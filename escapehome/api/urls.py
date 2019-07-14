096802477b4228694dddc9211983a293d9143e3b
jgiebelhausen <jgiebelhausen@seibert-media.net>
2019-07-14 15:51:22 +0200

    Added Api endpoint for Pi commands

--- escapehome/api/urls.py
+++ escapehome/api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from api.views import ready
+from api.views import ready, commands
 
 urlpatterns = [
     url(r'^ready/$', ready),
+    url(r'^commands/$', commands),
 ]
--- escapehome/api/views.py
+++ escapehome/api/views.py
@@ -1,7 +1,10 @@
+import json
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from api.serializers import ReadySerializer
+from escapehome import settings
 
 
 @api_view(['POST'])
@@ -12,3 +15,15 @@ def ready(request):
     print(serializer.validated_data['text'])
 
     return Response(status=200, data=["nichts!"])
+
+
+@api_view(['POST', 'GET'])
+def commands(request):
+    try:
+        with open(f'{settings.PROJECT_DIR}/escapehome/api/resources/protocol_commands.json') as json_file:
+            data = json.load(json_file)
+    except:
+        print("Fehler!")
+        data = ''
+
+    return Response(status=200, data=data)
