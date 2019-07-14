a5b5afa626c330174a5c4647553f7eae127cdac3
Anna Abad Sieper <anna.alicia@freenet.de>
2019-07-14 12:32:00 +0200

    adjust motor to box

--- box/motor.py
+++ box/motor.py
@@ -9,12 +9,12 @@ GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 50)
 
 try:
-	p.start(7.5)  # 90 Grad
+	p.start(6)
 	while True:
 		time.sleep(1)
-		p.ChangeDutyCycle(2.5)   # 0 Grad
+		p.ChangeDutyCycle(2.5)
 		time.sleep(1)
-		p.ChangeDutyCycle(12.5)  # 180 Grad
+		p.ChangeDutyCycle(6)
 
 except KeyboardInterrupt:
 	print(" Terminating..")
